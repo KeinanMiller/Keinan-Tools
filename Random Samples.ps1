c5993ae9d1d56d43d1bc50c5e6a7c5884439e7b2
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:15:33 -0800

    Delete Random Samples.ps1

--- Random Samples.ps1
+++
@@ -1,5 +0,0 @@
-
- $AutomaticVariables = Get-Variable
-function ShowVar {
-    Compare-Object (Get-Variable) $AutomaticVariables -Property Name -PassThru | Where -Property Name -ne "AutomaticVariables"
-}
