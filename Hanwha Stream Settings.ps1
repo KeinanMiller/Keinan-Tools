56beeaa3953643e42bf43806ac06345275df3960
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:54:55 -0800

    Update Hanwha Stream Settings.ps1

--- Hanwha Stream Settings.ps1
+++ Hanwha Stream Settings.ps1
@@ -1,10 +1,10 @@
-
+#this sets any camera Hanwha camera selected to 15 frames per second, adjusts streaming mode 
 Connect-ManagementServer -ShowDialog -Force -AcceptEula -ErrorAction Stop
 $camera = Select-Camera -Allowfolders -Allowservers -Title 'Select cameras'
 $stream = $camera | Get-VmsCameraStream -LiveDefault
 $settings = @{
     StreamingMode = "RTP_RTSP_TCP" #for Hanwha Techwin Cameras
-    Framerate = 12         #for Hanwha Techwin Cameras
+    Framerate = 15         #for Hanwha Techwin Cameras
     #FPS = 20           #for Axis Cameras
     #StreamingMode = "TCP"  #for Axis Cameras
     #Zstrength = "Medium" #for AXIS set Zipstream to medium
