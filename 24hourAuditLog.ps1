34455175b79f4ca1877cc78b78739c7cf2e1a3cf
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:29:22 -0800

    Update 24hourAuditLog.ps1

--- 24hourAuditLog.ps1
+++ 24hourAuditLog.ps1
@@ -1,2 +1,2 @@
-#used for pulling a milestone log of the 24 hours and export to csv
+#used for pulling a milestone log of the 24 hours and export to csv connecting to management server is required first.
 Get-Log -LogType Audit -Tail -Minutes 1440 | Export-Csv -Path .\auditLogs.csv -NoTypeInformation
