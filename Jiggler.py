6a2a814340a3476f584290c6cd64b376c08c213d
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 12:01:03 -0800

    Delete Jiggler.py

--- Jiggler.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import keyboard
-
-while True:
-    pyautogui.moveRel(5, 0, duration=0.1)
-    pyautogui.moveRel(-5, 0, duration=0.1)
-    if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-        break  # finishing the loopq
