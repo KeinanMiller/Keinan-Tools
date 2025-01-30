213939e1760a9d0aa7abca6646f03e874117eea2
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:29:52 -0800

    Update 1WeekAuditLog.ps1

--- 1WeekAuditLog.ps1
+++ 1WeekAuditLog.ps1
@@ -1,2 +1,2 @@
-#used for pulling a milestone log of the last week and export to csv
+#used for pulling a milestone log of the week and export to csv connecting to management server is required first.
 Get-Log -LogType Audit -Tail -Minutes 10080 | Export-Csv -Path .\auditLogs.csv -NoTypeInformation
