eeea45721950ca532adfcb40f3fb02576b3eb588
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:53:05 -0800

    Update GenericEvents.ps1

--- GenericEvents.ps1
+++ GenericEvents.ps1
@@ -1,3 +1,4 @@
+#used for Importing  Generic events in Milestone from a file see sample file in git titled ImportGenericEventsTemplates.CVS
 $newEvents = Import-CSV -path c:\installs\GenericEvents.csv
 
 Foreach ($event in $newEvents) {
