c9d4c5b62181a358ca77dbb1b6b39768d1fa7631
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:52:06 -0800

    Update UpdateGEimport.ps1

--- UpdateGEimport.ps1
+++ UpdateGEimport.ps1
@@ -1,4 +1,4 @@
-#used for updating  Generic events in Milestone from a file see sample file in git titled ImportGenericEventsTemplates.CVS
+#used for updating  Generic events in Milestone from a file see sample file in git titled ImportGenericEventsTemplates.CVS export of info is required
 foreach ($row in Import-Csv -Path c:\installs\GE.csv) {
     $GE = Get-GenericEvent | Where-Object {$_.Id -eq $row.Id} 
     $GE.Expression = $row.Expression
