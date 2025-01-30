f42ebc4f183d3ddc2aa5c79194ff70f525c4c28a
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:59:20 -0800

    Delete ImportHardwareNames.ps1

--- ImportHardwareNames.ps1
+++
@@ -1,7 +0,0 @@
-
-foreach ($row in Import-Csv -Path c:\installs\HardwareNamesImport.csv) {
-    $Hardware = Get-Hardware -Id $row.Id
-    $Hardware.Name = $row.Name
-    $Hardware.Description = $row.Description
-    $Hardware.Save()
-}
