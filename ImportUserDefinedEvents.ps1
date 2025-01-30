391cb81249ed935ed1e915c72219d6eace9887b9
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 12:00:36 -0800

    Update ImportUserDefinedEvents.ps1

--- ImportUserDefinedEvents.ps1
+++ ImportUserDefinedEvents.ps1
@@ -1,3 +1,4 @@
+#used for Creating User-defined events in Milestone from a file see sample file in git titled ImportUserEventsTemplates.CVS 
 Connect-ManagementServer -ShowDialog -AcceptEula
 $ImportEvents = Import-Csv -Path C:\installs\ImportUserEvents.csv | select UserEvent 
 $ImportEvents | foreach {
