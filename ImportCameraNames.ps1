65ca3ab45e91d4079a28109fcb30fa04489eef1b
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:58:57 -0800

    Update ImportCameraNames.ps1

--- ImportCameraNames.ps1
+++ ImportCameraNames.ps1
@@ -1,4 +1,4 @@
-
+#used for importing the changed named of the ExportCameraNames.ps1 file need updating for aliasing
 foreach ($row in Import-Csv -Path c:\installs\CameraNamesImport.csv) {
     $camera = Get-VMSCamera -Id $row.Id
     $camera.Name = $row.Name
