7d3a79021447f1ffa33ee2ada191e6524f0e82a4
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 12:06:26 -0800

    Update MilestoneClientDumpFolderCleanup.ps1

--- MilestoneClientDumpFolderCleanup.ps1
+++ MilestoneClientDumpFolderCleanup.ps1
@@ -1,1 +1,2 @@
+#when run deletes and dump files older that 7 days from the file location listed below.
 Get-ChildItem -Path "c:\ProgramData\Milestone\XProtect Management Client" -directory | Where-Object {$_.Name -like "dump-*" -and $_.LastWriteTime -lt (get-date).adddays(-7)}|remove-item -recurse
