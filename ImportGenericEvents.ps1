446e83e9e1c6b66de596f298f81cf4ee600b0f0c
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:54:11 -0800

    Update ImportGenericEvents.ps1

--- ImportGenericEvents.ps1
+++ ImportGenericEvents.ps1
@@ -1,4 +1,4 @@
-
+#used for updating  Generic events in Milestone from a file see sample file in git titled ImportGenericEventsTemplates.CVS simpilar to file named GenericEnvents --todo review and delete one
 Connect-ManagementServer -ShowDialog -AcceptEula
 $ImportEvents = Import-Csv -Path Get-FileName('c:\') | Select EventName, EventString
 $ImportEvents | ForEach {
