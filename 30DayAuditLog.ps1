56360855211f75cd1500fd312b22485c1bd00e27
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2025-01-30 11:30:16 -0800

    Update 30DayAuditLog.ps1

--- 30DayAuditLog.ps1
+++ 30DayAuditLog.ps1
@@ -1,1 +1,2 @@
+#used for pulling a milestone log of the 30 days and export to csv connecting to management server is required first.
 Get-Log -LogType Audit -Tail -Minutes 43200 | Export-Csv -Path .\auditLogs.csv -NoTypeInformation
