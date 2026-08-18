af905ad1bd6a322b0aa485082e0d78662b168e86
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2026-08-18 10:03:55 -0700

    Delete MSPS 24hourAuditLog.ps1

--- MSPS 24hourAuditLog.ps1
+++
@@ -1,2 +0,0 @@
-#used for pulling a milestone log of the 24 hours and export to csv connecting to management server is required first.
-Get-Log -LogType Audit -Tail -Minutes 1440 | Export-Csv -Path .\auditLogs.csv -NoTypeInformation
