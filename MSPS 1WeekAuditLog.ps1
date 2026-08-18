427093a234f278d63b4e1641efe5bfbe2aab0e4a
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2026-08-18 10:02:10 -0700

    Delete MSPS 1WeekAuditLog.ps1

--- MSPS 1WeekAuditLog.ps1
+++
@@ -1,2 +0,0 @@
-#used for pulling a milestone log of the week and export to csv connecting to management server is required first.
-Get-Log -LogType Audit -Tail -Minutes 10080 | Export-Csv -Path .\auditLogs.csv -NoTypeInformation
