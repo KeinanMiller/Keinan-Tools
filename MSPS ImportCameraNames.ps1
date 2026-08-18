a416281deccb189d458b1fc0d32ea03b1b3438b9
KeinanMiller <42756686+KeinanMiller@users.noreply.github.com>
2026-08-18 10:03:09 -0700

    Delete MSPS ImportCameraNames.ps1

--- MSPS ImportCameraNames.ps1
+++
@@ -1,7 +0,0 @@
-#used for importing the changed named of the ExportCameraNames.ps1 file need updating for aliasing
-foreach ($row in Import-Csv -Path c:\installs\CameraNamesImport.csv) {
-    $camera = Get-VMSCamera -Id $row.Id
-    $camera.Name = $row.Name
-    $camera.Description = $row.Description
-    $camera.Save()
-}
